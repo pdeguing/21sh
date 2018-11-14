0f92c49870061445edd8a6764f71156791666914
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2018-11-13 17:12:11 -0800

    Added newlines and quotes handling in parsing, nested quotes left to write

--- srcs/execute_tree.c
+++ srcs/execute_tree.c
@@ -6,7 +6,7 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/20 08:42:57 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/11/01 16:37:50 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/11/13 15:56:53 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,18 +48,6 @@ char		**get_args(t_tree **root)
 	return (args);
 }
 
-/*
-** I wanted to implement a stack here, keeping track of the fds used for each
-** command. It would have been interesting, but sadly it seems smarter to go
-** with a standard recursive method. We are parsing shell language, so the size
-** of the input and thus the resulting tree should always be manageable
-** within the stack. And using a manual stack would create an exceptional
-** case where we should always let stdin (or the fd replacing it after a
-** redirection) on top. This would lead to the necessicity to
-** implement two parallel stacks or to do some value swapping within
-** a stack storing an array of 2 fd, read end write end.
-*/
-
 void		execute_tree(t_tree **root, char flag, t_io *io_stack)
 {
 	t_tree	*head;
@@ -70,19 +58,6 @@ void		execute_tree(t_tree **root, char flag, t_io *io_stack)
 	head = *root;
 	if (!head)
 		return ;
-	/*
-	ft_printf(RED"CURRENT: "RESET"%s\n", g_strtype[head->token->type]);
-	if (head->left)
-	{
-		ft_printf(RED"left: "RESET);
-		ft_printf("%s > %s\n", g_strtype[head->left->token->type], head->left->token->literal);
-	}
-	if (head->right)
-	{
-		ft_printf(RED"right: "RESET);
-		ft_printf("%s > %s\n", g_strtype[head->right->token->type], head->right->token->literal);
-	}
-	*/
 	if (!head->left)
 		execute_cmd(get_args(&head), flag, &io_stack);
 	else if (head->token->type == SEMICOLON)
@@ -90,14 +65,6 @@ void		execute_tree(t_tree **root, char flag, t_io *io_stack)
 		execute_tree(&head->left, flag, io_stack);
 		execute_tree(&head->right, flag, io_stack);
 	}
-	/*
-	** The first action in the stack will be put by a PIPELINE
-	** So we don't actually want to modify the stack of the pipe
-	** between branch, we send a copy. Nevermind it's recursive
-	** we don't never modify it for the upper level. So we just
-	** push in the stack at each level of recursion until
-	** execution.
-	*/
 	else if (head->token->type == PIPELINE)
 	{
 		if (pipe(p) == -1)
@@ -106,18 +73,11 @@ void		execute_tree(t_tree **root, char flag, t_io *io_stack)
 			exit(EXIT_FAILURE);
 		}
 		io_cpy = io_stack_dup(io_stack);
-		/*
-		ft_printf(RED"pipe[%d, %d]\n"RESET, p[READ], p[WRITE]);
-		*/
 		execute_tree(&head->left, flag ^ WAIT, io_push(1, p[WRITE], io_stack, PIPELINE));
 		close(p[WRITE]);
 		execute_tree(&head->right, flag, io_push(0, p[READ], io_cpy, PIPELINE));
 		close(p[READ]);
 	}
-	/*
-	** Simple redirections will push in front the action of replacing
-	** one fd with the one resulting of the open call
-	*/
 	else if (head->token->type == GREAT)
 	{
 		fd = open(head->right->token->literal, O_WRONLY | O_CREAT, 0644);
--- srcs/token_get_word.c
+++ srcs/token_get_word.c
@@ -6,37 +6,67 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/25 11:25:11 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/11/13 15:05:18 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/11/13 17:10:42 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
+static char	*append_char(char *str, char c)
+{
+	int		size;
+	char	*new;
+
+	size = 1;
+	if (str)
+		size += ft_strlen(str);
+	new = ft_strnew(size + 1);
+	if (!new)
+		return (NULL);
+	new[size] = '\0';
+	if (str)
+		ft_strcpy(new, str);
+	ft_strdel(&str);
+	new[size - 1] = c;
+	return (new);
+
+}
+
+/*
+** Added append_char(), just need to manage quotes now, they will always be
+** correct based on readline, so we just need to remove the quotes character
+** and keep track of the state to know if we append newline or just skip
+** it
+*/
+
+/* Bash ignores nested quotes */
+
 int		token_get_word(char **pstr, char *pchar, t_token *token)
 {
 	int		quote;
 	char	*first_char;
-	int		i;
 
 	quote = 0;
-	token->type = TOKEN;
 	first_char = pchar;
-	i = 0;
+	token->type = TOKEN;
 	while (*pchar != '\0')
 	{
 		if (!quote && ft_strchr(";|<>", *pchar))
 			break ;
 		else if (!quote && ft_strchr(" 	", *pchar))
 			break ;
+		else if (*pchar == '\\')
+			quote ^= Q_BSLASH;
 		else if (*pchar == '\'')
-			quote ^= B_SQUOTE;
+			quote ^= Q_SQUOTE;
 		else if (*pchar == '\"')
-			quote ^= B_DQUOTE;
+			quote ^= Q_DQUOTE;
+		else if (!(quote & Q_BSLASH) || *pchar != '\n')
+		{
+			token->literal = append_char(token->literal, *pchar);
+		}
 		pchar++;
-		i++;
 	}
-	token->literal = ft_strnew(i);
-	ft_memcpy(token->literal, first_char, i);
 	*pstr = pchar;
 	return (1);
 }
