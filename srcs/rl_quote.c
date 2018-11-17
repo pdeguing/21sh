c7c5f7834594fa8f48e9504ca0c133982ee66626
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2018-11-16 16:26:52 -0800

    Ready for exceptions checking and debugging

--- srcs/rl_quote.c
+++ srcs/rl_quote.c
@@ -6,13 +6,11 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 14:39:01 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/11/16 12:54:59 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/11/16 13:24:41 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
-
-#define CHECK(status, mask) (status & mask)
 
 /*
 ** Returns 0 if all quotes are closed (the next newline will not be quoted).
@@ -34,11 +32,11 @@ int		rl_quote(t_rl *rl)
 		j = 0;
 		while (str && str[j])
 		{
-			if (str[j] == '\'' && !CHECK(status, (Q_BSLASH | Q_DQUOTE)))
+			if (str[j] == '\'' && !(status & (Q_BSLASH | Q_DQUOTE)))
 				status ^= Q_SQUOTE;
-			if (str[j] == '"' && !CHECK(status, (Q_BSLASH | Q_SQUOTE)))
+			if (str[j] == '"' && !(status & (Q_BSLASH | Q_SQUOTE)))
 				status ^= Q_DQUOTE;
-			if (str[j] == '\\' && (!CHECK(status, (Q_BSLASH | Q_SQUOTE)) || (CHECK(status, Q_DQUOTE) && ft_strchr("\"\\\n", rl->key))))
+			if (str[j] == '\\' && (!(status & (Q_BSLASH | Q_SQUOTE)) || ((status & Q_DQUOTE) && ft_strchr("\"\\\n", rl->key))))
 				status |= Q_BSLASH;
 			else
 				status &= ~Q_BSLASH;
