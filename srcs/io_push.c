36f8398c0cbf8e0fe09376238b3ce02d56cb19de
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2018-11-21 08:25:05 -0800

    Finally found the law of redirection

--- srcs/io_push.c
+++ srcs/io_push.c
@@ -6,7 +6,7 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/24 12:43:54 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/11/20 18:42:26 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/11/21 08:24:11 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,16 @@ t_io	*io_push(int dst, int src, t_io *io_stack, int op)
 	if (!io_stack)
 		return (new);
 	head = io_stack;
-	while (head->next)
+	while (head->op == PIPELINE)
 	{
+		if (!head->next || head->next->op != PIPELINE)
+		{
+			new->next = head->next;
+			head->next = new;
+			return (io_stack);
+		}
 		head = head->next;
 	}
-	head->next = new;
-	return (io_stack);
+	new->next = head;
+	return (new);
 }
--- srcs/io_redirect.c
+++ srcs/io_redirect.c
@@ -6,22 +6,11 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/24 10:16:32 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/11/20 18:42:26 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/11/21 08:24:28 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
-
-/*
-** So we could actually use a stack of duplications to do for a specific
-** stage in recursion, or rather an array of fd to duplicate with their
-** destination at each level so that at the end we execute with:
-** 			-> replace 0, per 3
-**			-> replace 1, per 4
-**			-> replace 2, per 1
-**			-> replace 3, per 6
-** or whatever.
-*/
 
 void	io_redirect(t_io *io_stack)
 {
@@ -30,8 +19,6 @@ void	io_redirect(t_io *io_stack)
 	head = io_stack;
 	while (head)
 	{
-		ft_printf(RED"{op = %d, src = %d, dst = %d}\n"RESET, head->op, head->src, head->dst);
-		/*
 		if (head->op == GREATAND || head->op == LESSAND)
 		{
 			if (head->src == '-')
@@ -41,7 +28,6 @@ void	io_redirect(t_io *io_stack)
 				continue ;
 			}
 		}
-		*/
 		if (dup2(head->src, head->dst) == -1)
 		{
 			perror("21sh");
